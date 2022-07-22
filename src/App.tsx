5e9f5290e8284885b80b86e16d2995ca947d3c01
Cody (Wen) Huang <whuang17@mail.sfsu.edu>
2022-07-22 11:27:06 -0700

    parsed zone names

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// fetch('http://jump.javin.io:5000/api/zones')
+//   .then(response => response.json())
+//   .then(obj => console.log(obj.data[0][1].name));
+
+
 fetch('http://jump.javin.io:5000/api/zones')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(obj => {
+    var zones = obj.data;
+    const keys = Object.keys(zones);
+    //const zoneID = Object.keys(zones[keys])
+    //console.log(zones);
+    //console.log(keys);
+    const str: String = 'name';
+    keys.forEach((key) => {
+      var zoneID = Object.keys(zones[key]);
+      //console.log(zoneID);
+      console.log(zones[key][zoneID.toString()].name);
 
-function App() {
+    })
+  });
+
+function App() {  
   return (
     <div className="App">
       <header className="App-header">
