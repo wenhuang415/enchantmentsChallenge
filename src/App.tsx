41bfb30e87a4124768a5e1f9dc038a65f2c68879
Cody (Wen) Huang <whuang17@mail.sfsu.edu>
2022-07-22 16:00:16 -0700

    fix image src issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
 import './App.css';
 import Cards from './Components/Cards';
 
-fetch('http://jump.javin.io:5000/api/zones')
-  .then(response => response.json())
-  .then(obj => {
-    var zones = obj.data;
-    const keys = Object.keys(zones);
-    //const zoneID = Object.keys(zones[keys])
-    //console.log(zones);
-    //console.log(keys);
-    keys.forEach((key) => {
-      var zoneID = Object.keys(zones[key]);
-      //console.log(zoneID);
-      console.log(zones[key][zoneID.toString()].name);
-    })
-  });
-
 function App() {  
   return (
     <div className="App">
--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -9,47 +9,59 @@ import {
   Button,
 } from "@mantine/core";
 
+function getZones(){
+    var zoneName: any[] = [];
+    fetch('http://jump.javin.io:5000/api/zones')
+    .then(response => response.json())
+    .then(obj => {
+        var zones = obj.data;
+        const keys = Object.keys(zones);
+        keys.forEach((key) => {
+        var zoneID = Object.keys(zones[key]);
+        zoneName.push(zones[key][zoneID.toString()].name);
+        })
+    });
+    return zoneName;
+}
+
+const zones = getZones();
+//console.log(zones);
+console.log(zones);
+
+
+
 function Cards() {
   const theme = useMantineTheme();
   return (
     <div className="App">
       <Grid justify="space-around">
         <Grid.Col style={{ maxWidth: 300 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
-            <Card.Section>
-              <Image src="./colchuck.jpeg" height={160} alt="Norway" />
-            </Card.Section>
+                <Card shadow="sm" p="lg">
+        <Card.Section>
+          <Image src="./colchuck.png" height={160} alt="Norway" />
+        </Card.Section>
 
-            <Group
-              position="apart"
-              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-            >
-              <Text weight={500}>Norway Fjord Adventures</Text>
-              <Badge color="pink" variant="light">
-                On Sale
-              </Badge>
-            </Group>
+        <Group position="apart" style={{ marginBottom: 5, marginTop: theme.spacing.sm }}>
+          <Text weight={500}>Norway Fjord Adventures</Text>
+          <Badge color="pink" variant="light">
+            On Sale
+          </Badge>
+        </Group>
 
-            <Text size="sm" style={{ lineHeight: 1.5 }}>
-              With Fjord Tours you can explore more of the magical fjord
-              landscapes with tours and activities on and around the fjords of
-              Norway
-            </Text>
+        <Text size="sm" style={{  lineHeight: 1.5 }}>
+          With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+          activities on and around the fjords of Norway
+        </Text>
 
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Book classic tour now
-            </Button>
-          </Card>
+        <Button variant="light" color="blue" fullWidth style={{ marginTop: 14 }}>
+          Book classic tour now
+        </Button>
+      </Card>
         </Grid.Col>
         <Grid.Col style={{ maxWidth: 300 }} sm={4} xs={4}>
           <Card shadow="sm" p="lg">
             <Card.Section>
-              <Image src="./../../public/colchuck.jpeg" height={160} alt="Norway" />
+              <Image src="./coreEnchantment.jpeg" height={160} alt="Norway" />
             </Card.Section>
 
             <Group
