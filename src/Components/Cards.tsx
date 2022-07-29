17a62bba5dd128ca61f007826d0b8cf8ed6a440c
Cody (Wen) Huang <whuang17@mail.sfsu.edu>
2022-07-28 19:34:54 -0700

    reformated cards

--- src/Components/Cards.tsx
+++ src/Components/Cards.tsx
@@ -5,207 +5,242 @@ import {
   Group,
   Text,
   Button,
+  Image,
 } from "@mantine/core";
-import Award from './Modal';
+import Award from "./Modal";
 
-document.addEventListener('DOMContentLoaded', ()=>{ 
+document.addEventListener("DOMContentLoaded", () => {
   function fetchZones() {
-    fetch('http://jump.javin.io:5000/api/zones')
-    .then(response => response.json())
-    .then( data => getZones(data))
-    .catch(Error);
+    fetch("http://jump.javin.io:5000/api/zones")
+      .then((response) => response.json())
+      .then((data) => getZones(data))
+      .catch(Error);
   }
 
-  function getZones(zones:any){
-    const keys = Object.keys(zones.data)
-    console.log(keys)
+  function getZones(zones: any) {
+    const keys = Object.keys(zones.data);
+    console.log(keys);
     let i = 1;
     keys.forEach((key) => {
-      let textID = '#zone'+i
-      const zone = document.querySelector(textID)
-      const text = document.createElement('Text')
-      text.innerHTML = zones.data[key][i].name
-      zone?.append(text)
-      i++
-    })
+      //create a card component to display zone name
+      // const gridCol = document.createElement('Grid.Col')
+      // gridCol.style{{ paddingTop: 40, maxWidth: 300, minHeight: 400 }} sm={4} xs={4}'
+
+
+      let textID = "#zone" + i;
+      const zone = document.querySelector(textID);
+      const text = document.createElement("Text");
+      text.innerHTML = zones.data[key][i].name;
+      zone?.append(text);
+      i++;
+    });
   }
   fetchZones();
-})
+});
 
 function Cards() {
   const theme = useMantineTheme();
-  theme.colorScheme = 'light';
+  theme.colorScheme = "light";
   return (
-    <div className="App" >
-      <Grid  id= 'grid1' justify="space-around">
-        
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
+    <div className="App">
+      <Grid id="grid1" justify="space-around">
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
           <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
             <Group
               position="apart"
               style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              <Text id='zone1' weight={500}></Text>
+              <Text id="zone1" weight={500}></Text>
+              
             </Group>
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
-          </Card>
-        </Grid.Col>
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
-            <Group
-              position="apart"
-              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-            >
-              <Text id='zone2' weight={500}></Text>
-            </Group>
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
-          </Card>
-        </Grid.Col>
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
-            <Group
-              position="apart"
-              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-            >
-              <Text id='zone3' weight={500}></Text>
-            </Group>
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
-          </Card>
-        </Grid.Col>
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
-            <Group
-              position="apart"
-              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-            >
-              <Text id='zone4' weight={500}></Text>
-            </Group>
-
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
+            <Award />
           </Card>
         </Grid.Col>
 
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
             <Group
               position="apart"
               style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              <Text id='zone5' weight={500}></Text>
+              <Text id="zone2" weight={500}></Text>
+              
             </Group>
-
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
+            <Award />
           </Card>
         </Grid.Col>
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
+
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
             <Group
               position="apart"
               style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              <Text id='zone6' weight={500}></Text>
+              <Text id="zone3" weight={500}></Text>
+              
             </Group>
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
+            <Award />
           </Card>
         </Grid.Col>
-        <Grid.Col style={{paddingTop: 40, maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
+
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
             <Group
               position="apart"
               style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              <Text id='zone7' weight={500}></Text>
+              <Text id="zone4" weight={500}></Text>
+              
             </Group>
-
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
-            >
-              Get Award Data
-            </Button>
-            <Award/>
+            <Award />
           </Card>
         </Grid.Col>
-        <Grid.Col style={{ paddingTop: 40,maxWidth: 300,minHeight:400 }} sm={4} xs={4}>
-          <Card shadow="sm" p="lg">
+
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
             <Group
               position="apart"
               style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              <Text id='zone8' weight={500}></Text>
+              <Text id="zone5" weight={500}></Text>
+              
             </Group>
+            <Award />
+          </Card>
+        </Grid.Col>
 
-
-            <Button
-              variant="light"
-              color="blue"
-              fullWidth
-              style={{ marginTop: 14 }}
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
+            <Group
+              position="apart"
+              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
             >
-              Get Award Data
-            </Button>
-            <Award/>
+              <Text id="zone6" weight={500}></Text>
+              
+            </Group>
+            <Award />
+          </Card>
+        </Grid.Col>
+
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
+            <Group
+              position="apart"
+              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+            >
+              <Text id="zone7" weight={500}></Text>
+              
+            </Group>
+            <Award />
+          </Card>
+        </Grid.Col>
+
+        <Grid.Col
+          style={{ paddingTop: 40, maxWidth: 300, minHeight: 500 }}
+          sm={4}
+          xs={4}
+        >
+          <Card color="blue" shadow="sm" p="lg">
+            <Card.Section>
+              <Image
+                src="https://images.squarespace-cdn.com/content/v1/5346a846e4b0101b157468cf/1487721949963-CNJL4DDSSV7VRB3HS66Z/15401560621_9c96b7b02a_o.jpg?format=1000w"
+                height={160}
+                alt="Enchantment Lake"
+              />
+            </Card.Section>
+            
+            <Group
+              position="apart"
+              style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+            >
+              <Text id="zone8" weight={500}></Text>
+              
+            </Group>
+            <Award />
           </Card>
         </Grid.Col>
       </Grid>
--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -3,6 +3,10 @@ import { Modal, Button, Group, ScrollArea } from "@mantine/core";
 const randomtext =
   "Charizard description from BulbapediaCharizard is a draconic, bipedal Pokémon. It is primarily orange with a cream underside from the chest to the tip of its tail. It has a long neck, small blue eyes, slightly raised nostrils, and two horn-like structures protruding from the back of its rectangular head. There are two fangs visible in the upper jaw when its mouth is closed. Two large wings with blue-green undersides sprout from its back, and a horn-like appendage juts out from the top of the third joint of each wing. A single wing-finger is visible through the center of each wing membrane. Charizard's arms are short and skinny compared to its robust belly, and each limb has three white claws. It has stocky legs with cream-colored soles on each of its plantigrade feet. The tip of its long, tapering tail burns with a sizable flame.As Mega Charizard X, its body and legs are more physically fit, though its arms remain thin. Its skin turns black with a sky-blue underside and soles. Two spikes with blue tips curve upward from the front and back of each shoulder, while the tips of its horns sharpen, turn blue, and curve slightly upward. Its brow and claws are larger, and its eyes are now red. It has two small, fin-like spikes under each horn and two more down its lower neck. The finger disappears from the wing membrane, and the lower edges are divided into large, rounded points. The third joint of each wing-arm is adorned with a claw-like spike. Mega Charizard X breathes blue flames out the sides of its mouth, and the flame on its tail now burns blue. It is said that its new power turns it black and creates more intense flames.";
 
+function modd(){
+  console.log('hihi')
+}
+
 function Award() {
   const [opened, setOpened] = useState(false);
 
@@ -17,16 +21,10 @@ function Award() {
       </Modal>
 
       <Group position="center">
-        <Button onClick={() => setOpened(true)}>Open Modal</Button>
+        <Button onClick={() => setOpened(true)}>Awards</Button>
       </Group>
     </>
   );
 }
-
-// function Award() {
-//     return (
-//         <Button>Click for Awards</Button>
-//     )
-// }
 
 export default Award;
